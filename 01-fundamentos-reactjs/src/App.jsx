f1a4632ac93723d32b7dbbffad539e1182a5fa35
Leonardo Trindade <ltrindade@inf.ufsm.br>
2023-05-05 16:45:29 -0300

    wip: started to create a sidebar component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,15 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1550063873-ab792950096b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <strong>Leonardo Trindade</strong>
+        <span>Software Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
