16a9815f120952f01fcba084af240ac33bae88a7
Leonardo Trindade <ltrindade@inf.ufsm.br>
2023-05-08 15:05:29 -0300

    Created a base component for posts

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 import styles from "./App.module.css";
 import "./global.css";
 
@@ -9,6 +10,9 @@ export function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
+        <main>
+          <Post />
+        </main>
       </div>
     </div>
   );
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,35 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/ltrindadebr.png" />
+          <div className={styles.authorInfo}>
+            <strong>Leonardo Trindade</strong>
+            <span>Software Engineer</span>
+          </div>
+        </div>
+
+        <time title="08 de Maio às 11:20h" dateTime="2023-05-08 11:20:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala pessoal 👋</p>
+        <p>Primeiro projetinho do React Ignite da Rocketseat saindo!</p>
+        <p>
+          👉{" "}
+          <a href="https://github.com/ltrindadebr" target="_blank">
+            github.com/ltrindadebr
+          </a>
+        </p>
+        <p>
+          <a href="#">#ignite</a> <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
