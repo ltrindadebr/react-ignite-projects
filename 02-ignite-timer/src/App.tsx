4413ae166363550e13e3943891ff88cfecd202ee
Leonardo Trindade <ltrindade@inf.ufsm.br>
2023-09-18 21:24:04 -0300

    Adds react router dom and create basic pages

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,19 +1,18 @@
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="success" />
-        <Button variant="danger" />
-        <Button />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <GlobalStyle />
       </ThemeProvider>
     </>
-  )
+  );
 }
