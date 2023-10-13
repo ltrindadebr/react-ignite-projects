942fb699826ecdaef52e5d10806259a6d1df3c21
Leonardo Trindade <leonardo.trindade@take.net>
2023-10-13 15:08:07 -0300

    Adds the base of timer at home page

--- 02-ignite-timer/src/styles/global.ts
+++ 02-ignite-timer/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -9,12 +9,13 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: none;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
   }
 
   body {
-    background: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']};
+    background: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
@@ -22,4 +23,4 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 400;
     font-size: 1rem;
   }
-`
+`;
